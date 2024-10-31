c60701b2b25e60f34d4ab2877663f77e51f268d3
82orez <82orez@naver.com>
2024-11-01 05:22:50 +0900

    add ui.tsx

---
+++ src/app/ui.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Ui() {
+  return (
+    <div className={"border-2 w-2/3 mx-auto flex flex-col items-center py-10"}>
+      <h1 className={"text-xl"}>TODO LIST</h1>
+    </div>
+  );
+}
